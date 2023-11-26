995429ecec078a83a821c0014f2f0e1c043ebf01
Chris Lime <chrkalivas@gmail.com>
2023-11-27 01:45:00 +0200

    add prettier prettier tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,22 @@ import {Inter as FontSans} from 'next/font/google'
 import {cn} from '@/lib/utils'
 
 export const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
+	subsets: ['latin'],
+	variable: '--font-sans',
 })
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased dark',
-          fontSans.variable
-        )}>
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={cn(
+					'dark min-h-screen bg-background font-sans antialiased',
+					fontSans.variable,
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	)
 }
